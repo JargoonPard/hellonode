17f0409f05055fadcdf6c43e33a0d3b92754515d
jargoonpard <larry.guger@hotmail.com>
2016-09-16 14:04:02 -0700

    Test label application in docker build system

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
-    response.end('Hello Kubernetes V2<br>');
+    response.end('Hello Kubernetes V2<br>New push to see what happens with the container label in docker build.');
 };
 
 var www = http.createServer(handleRequest);
